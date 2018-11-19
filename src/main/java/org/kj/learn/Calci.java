66bdd67156e3fb6f72470ab1a415afc4e0ce97ff
Kshama Jwala Kumar <j.kshama@gmail.com>
2018-11-18 23:28:50 -0800

    adding unit tests

---
+++ src/main/java/org/kj/learn/Calci.java
@@ -0,0 +1,13 @@
+package org.kj.learn;
+
+public class Calci {
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static int subtract(int a, int b) {
+    return a - b;
+  }
+
+}
---
+++ src/test/java/org/kj/learn/CalciTest.java
@@ -0,0 +1,29 @@
+package org.kj.learn;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalciTest {
+
+  @Test
+  public void addWhenNumberArePositive() {
+    assertEquals(5, Calci.add(2, 3));
+  }
+
+  @Test
+  public void addWhenNumbersAreNegative() {
+    assertEquals(-5, Calci.add(-2, -3));
+  }
+
+  @Test
+  public void addWhenNumbersAreZero() {
+    assertEquals(0, Calci.add(0, 0));
+  }
+
+  @Test
+  public void subtract() {
+    assertEquals(3, Calci.subtract(5, 2));
+  }
+
+}
